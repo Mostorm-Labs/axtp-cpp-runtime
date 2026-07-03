9fe2172ad99185772046c9b9e509e524df8cf062
luckievan <136231429+luckievan@users.noreply.github.com>
2026-07-03 02:59:20 +0000

    chore: upgrade AXTP Spec to spec/v0.11.2

--- include/core/protocol/generated/axtp_generated_version.hpp
+++ include/core/protocol/generated/axtp_generated_version.hpp
@@ -4,12 +4,12 @@
 namespace axtp::generated {
 
 inline constexpr const char* kRuntimeName = "axtp-cpp-runtime";
-inline constexpr const char* kRuntimeVersion = "0.11.1.0";
-inline constexpr const char* kSpecVersion = "0.11.1";
-inline constexpr const char* kSpecTag = "spec/v0.11.1";
-inline constexpr const char* kSpecCommit = "5efc24113572d38a60cf7cef73b54786c8282019";
+inline constexpr const char* kRuntimeVersion = "0.11.2.0";
+inline constexpr const char* kSpecVersion = "0.11.2";
+inline constexpr const char* kSpecTag = "spec/v0.11.2";
+inline constexpr const char* kSpecCommit = "be24f49f40e900f6f2ea8a4d69555fea1fcc3f62";
 inline constexpr const char* kGeneratorName = "@axtp/cpp-runtime-generator";
 inline constexpr const char* kGeneratorVersion = "1.0.0";
-inline constexpr const char* kGeneratedAt = "2026-07-03T02:06:23Z";
+inline constexpr const char* kGeneratedAt = "2026-07-03T02:57:42Z";
 
 }  // namespace axtp::generated
